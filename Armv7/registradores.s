e12a96a338cbedc69bc2fbc5ec60760f88c03c1e
Pablo Bezerra <pablobzerrads@gmail.com>
2026-04-07 23:49:03 -0300

    Create registradores.s

---
+++ Armv7/registradores.s
@@ -0,0 +1,8 @@
+um registrador é um conjunto de 32bits exemplos 
+```
+mov r0, #10 
+registrador r0 contem o numero 10 que em bits 00000000 00000000 00000000 00001010 
+```
+os registradores fica em CPU direto com hardware 
+
+existe 15 registradores
